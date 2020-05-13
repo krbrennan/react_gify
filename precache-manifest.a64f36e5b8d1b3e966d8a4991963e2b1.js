23c271d6bb1d12745d8cdac2abc747ea35771079
krbrennan <krb.brennan@gmail.com>
2020-05-13 15:44:23 -0400

    Updates

---
+++ precache-manifest.a64f36e5b8d1b3e966d8a4991963e2b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53d8a908f7f69549d3c04a0f1f14c7f",
+    "url": "/react_gify/index.html"
+  },
+  {
+    "revision": "a54c783e613cb1c10fba",
+    "url": "/react_gify/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "45faff490231ac672b07",
+    "url": "/react_gify/static/css/main.c7fa7d4e.chunk.css"
+  },
+  {
+    "revision": "a54c783e613cb1c10fba",
+    "url": "/react_gify/static/js/2.937e3471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_gify/static/js/2.937e3471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45faff490231ac672b07",
+    "url": "/react_gify/static/js/main.869107b4.chunk.js"
+  },
+  {
+    "revision": "0b53b633f163e2e1f7ab",
+    "url": "/react_gify/static/js/runtime-main.3da7fedd.js"
+  }
+]);
